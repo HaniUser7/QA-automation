521e37f83d798f5c386d06feb9926af276c841cd
hani1991 <hani@websitetoolbox.com>
2016-12-08 11:04:42 +0530

    resolved issues of get-attribute in loginByprivacyOption

--- automationScripts/testsuite/cases/loginByPrivacyOption.js
+++ automationScripts/testsuite/cases/loginByPrivacyOption.js
@@ -2,7 +2,7 @@ var config = require('../../../config/config.json');
 var json = require('../../testdata/inContextLogin.json');
 var loginPrivacyOptionMethod = require('../methods/loginByPrivacyOption.js');
 var inContextLoginMethod=require('../methods/inContextLogin.js');
-
+var utils=require('../utils.js')
 loginPrivacyOptionTests= module.exports = {};
 var wait=require('../wait.js');
 
@@ -23,7 +23,8 @@ loginPrivacyOptionTests.loginPrivacyOption=function(){
 					casper.capture('222.png');
 					casper.click('button.button.btn-m.btn-blue');
 					casper.thenOpen(config.url, function() {
-						
+					
+	
 					});					
 				}
 			});	
@@ -36,7 +37,7 @@ loginPrivacyOptionTests.loginPrivacyOption=function(){
 loginPrivacyOptionTests.loginPrivacylaunchapp=function() {
 	casper.thenOpen(config.url ,function(){
 		casper.echo('*********Check login while launching app****************','INFO');
-		inContextLoginMethod.loginToApp(json['validInfo'].username, json['validInfo'].password, casper, function(err) {
+		inContextLoginMethod.loginToApp(json['validInfos'].username, json['validInfos'].password, casper, function(err) {
 			if (err) {
 					casper.echo("Error occurred in callback user not logged-in", "ERROR");
 			}else {
@@ -65,7 +66,7 @@ loginPrivacyOptionTests.loginPrivacyFrmMainMenu=function() {
 		casper.echo('*********Check Login from Topic option in side menu****************','INFO');	
 		casper.click('i.icon.icon-menu');
 		casper.click('li#latest_topics_show a');
-		inContextLoginMethod.loginToApp(json['validInfo'].username, json['validInfo'].password, casper, function(err) {
+		inContextLoginMethod.loginToApp(json['validInfos'].username, json['validInfos'].password, casper, function(err) {
 			if (err) {
 					casper.echo("Error occurred in callback user not logged-in", "ERROR");
 			}else {
@@ -95,7 +96,7 @@ loginPrivacyOptionTests.loginPrivacyFrmMember=function() {
 		casper.click('i.icon.icon-menu');
 		casper.click('li#members_list_show a');
 		casper.click('li#latest_topics_show a');
-		inContextLoginMethod.loginToApp(json['validInfo'].username, json['validInfo'].password, casper, function(err) {
+		inContextLoginMethod.loginToApp(json['validInfos'].username, json['validInfos'].password, casper, function(err) {
 			if (err) {
 					casper.echo("Error occurred in callback user not logged-in", "ERROR");
 			}else {
@@ -121,12 +122,77 @@ loginPrivacyOptionTests.loginPrivacyFrmMember=function() {
 //calender id require in this testcase
 
 loginPrivacyOptionTests.loginPrivacyFrmCalender=function() {
-	casper.thenOpen(config.url ,function(){		
-		casper.echo('***********Check Login from Calender option is side menu**************','INFO');	
-		casper.click('i.icon.icon-menu');
-		casper.click('ul#calendars_toggle_link');
-		
-	});
+	casper.thenOpen(config.backEndUrl ,function(){			
+		casper.echo('***********Check Login from calender option in side menu**************','INFO');		
+		loginPrivacyOptionMethod.loginToForumBackEnd(casper , function(err) {
+			if (!err)
+				casper.echo('LoggedIn to forum backend....', 'INFO');
+		});
+		wait.waitForElement('div#my_account_forum_menu a[data-tooltip-elm="ddUsers"]', casper , function(err, isExists) {
+			if(isExists) {
+				casper.evaluate(function() {
+					document.querySelector('div#my_account_forum_menu a[data-tooltip-elm="ddUsers"').click();
+				});
+				wait.waitForElement('div.tooltipMenu.text a[title="Assign permissions to user groups"]', casper , function(err , isExists) {
+					if( isExists) {
+						casper.evaluate(function() {
+							document.querySelector('div.tooltipMenu.text a[title="Assign permissions to user groups"]').click();
+						});
+						wait.waitForTime(1000 , casper , function(err) {
+							var grpName = casper.evaluate(function(){
+	       								for(var i=1; i<=7; i++) {
+	        								var x1 = document.querySelector('tr:nth-child('+i+') td:nth-child(1)');
+										if (x1.innerText == 'Unregistered / Not Logged In') {
+											document.querySelector('tr:nth-child('+i+') td:nth-child(3) a').click();
+											var x2 = document.querySelector('tr:nth-child('+i+') td:nth-child(3) div.tooltipMenu a').getAttribute('href');
+											return x2;
+										}
+									}
+								});
+								casper.echo("message : "+grpName, 'INFO');
+								casper.click('div.tooltipMenu a[href="'+grpName+'"]');
+								wait.waitForElement('button.button.btn-m.btn-blue' , casper , function (err , isExists) {
+									if(isExists) {
+										utils.enableorDisableCheckbox('view_calendar',false, casper, function(err) {
+											if(!err)
+												casper.echo('Successfully unchecked','INFO');
+										});										
+										casper.thenOpen(config.url , function() {
+											wait.waitForElement('i.icon.icon-menu' ,casper , function (err ,isExists) {
+												if(isExists) {
+													casper.click('i.icon.icon-menu');
+													casper.click('li#calenders_show a');
+
+													inContextLoginMethod.loginToApp(json['validInfos'].username, json['validInfos'].password, casper, function(err) {
+														if (err) {
+															casper.echo("Error occurred in callback user not logged-in", "ERROR");
+														}else {
+															casper.echo('Processing to Login on forum.....', 'INFO');
+															wait.waitForTime(1000 , casper , function(err){
+															wait.waitForElement('ul.nav.pull-right span.caret', casper , function(err, isExists){
+																if(isExists) {
+															inContextLoginMethod.logoutFromApp(casper, function(err){
+																	if (!err)
+																	casper.echo('Successfully logout from application', 'INFO');
+																});
+																	}else {
+																	casper.echo('Toggle selector not found after login ul.nav.pull-right span.caret','ERROR');
+																	}
+																});
+			      	 											});
+														}
+													});
+												}						
+											});
+										});
+									}		
+								});
+							});
+						}
+					});
+				}
+			});
+		});
 };
 
 //loginPrivacyOptionTests.loginPrivacyFrmDonate
@@ -144,14 +210,16 @@ loginPrivacyOptionTests.loginPrivacyDonate=function() {
 				wait.waitForElement('input#paypal_email_address', casper , function(err , isExists) {
 					if(isExists) {
 						casper.fill('form#frmForumSettings',{
-							'paypal_email_address':'support@websitetoolbox.com'
+							'paypal_email_address':'support@websitetoolbox.com',
+							 'paid_access_donation':'1'
+							
 						},false);
 						casper.click('select#paid_access_area');
 						casper.click('button.button.btn-m.btn-blue');
 						casper.thenOpen(config.url, function() {
 							casper.click('i.icon.icon-menu');
 							casper.click('a#paid_access_donate_link');
-							inContextLoginMethod.loginToApp(json['validInfo'].username, json['validInfo'].password, casper, function(err) {
+							inContextLoginMethod.loginToApp(json['validInfos'].username, json['validInfos'].password, casper, function(err) {
 								if (err) {
 									casper.echo("Error occurred in callback user not logged-in", "ERROR");
 								}else {
